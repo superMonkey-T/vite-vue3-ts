55ca16b5873e1af9ae5334afa592eafa4f84f7c2
tcw <1096431375@qq.com>
2021-12-01 18:07:19 +0800

    测试提交iframe通信

--- src/views/Iframe.vue
+++ src/views/Iframe.vue
@@ -1,3 +1,15 @@
 <template>
   ifreme文件
+  <button @click="changeParent">触发父组件</button>
 </template>
+<script setup lang="ts">
+  const handleMessage = () => {
+    console.log('父组件穿过来的消息')
+  }
+  window.addEventListener('message' ,handleMessage)
+  const changeParent = () => {
+    window.parent.postMessage({
+      iframeSuccess: true
+    }, '*')
+  }
+</script>
