7ef41a603ef2adf23cac3b44e84556b2cd8eabd8
tcw <1096431375@qq.com>
2022-01-18 14:54:30 +0800

    iframe测试

--- src/views/Iframe.vue
+++ src/views/Iframe.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe src="https://test.industry.pharmconverge.com/industryNew/index.html#/purchase" width="100%" height="100%"></iframe>
+  <iframe src="https://test.industry.pharmconverge.com/industryNew/index.html#/purchase" frameborder="0" width="100%" height="100%"></iframe>
 </template>
 <script setup lang="ts">
 window.addEventListener('message',function(e){
